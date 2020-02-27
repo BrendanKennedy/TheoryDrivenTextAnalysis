f508a8b88cf7debf1626b8f01e9fc618a00d0ebc
BrendanKennedy <btkenned@usc.edu>
2020-02-27 17:46:04 -0600

    Finalizing GloVe and DDR R script

--- wordvecs.R
+++ wordvecs.R
@@ -2,72 +2,70 @@ library(tidyverse)
 library(broom)
 library(text2vec)
 library(tidytext)
-#library(knitr)
-#library(ggfortify)
 
-# function definitions --------------------------------------------------------------------------
-
-# input .txt file, exports list of list of values and character vector of names (words)
-proc_pretrained_vec <- function(p_vec, vocab, size_vec=100) {
-    # initialize space for values and the names of each word in vocab
-    vals <- vector(mode = "list", length(vocab))
-    names <- vector(mode = "character", length(vocab))
-    added <- 0
-    # loop through to gather values and names of each word
-    for(i in 1:length(p_vec)) {
-        if(i %% 1000 == 0) {
-            print(i)
-        }
-        if(added == length(vocab)) {
-            break;  # early termination
-        }
-        this_vec <- p_vec[i]
-        this_vec_unlisted <- unlist(strsplit(this_vec, " "))
-        this_vec_name <- this_vec_unlisted[1]
-        index <- match(this_vec_name, vocab)
-
-        if(this_vec_name %in% vocab) {
-            this_vec_values <- as.numeric(this_vec_unlisted[-1]) 
-            vals[[index]] <- this_vec_values
-            added <- added + 1
-            names[[index]] <- this_vec_name
-        }
-    }
-    
-    v <- rep(0.0, size_vec)
-    for(i in 1:length(names)) {
-        word <- vocab[[i]]
-        embed_word <- names[[i]]
-        if(word != embed_word) {
-            vals[[i]] <- v  # zero vector
-            names[[i]] <- word
-        }
-    }
-    glove <- data.frame(vals)
-    names(glove) <- names
-    glove <- as.data.frame(t(glove))
-    return(glove)
-}
-
-# tidy_doc is a tidytext formatted tibble with (word + doc) indices. 
-# dictionary is a tibble with a "sentiment" column; all other columns are glove means
-ddr <- function(tidy_doc, dictionary) {
-    this_wv_mat <- matrix(this_wv, ncol=length(this_wv), nrow=1)
-    all_wvs_mat <- as.matrix(all_wvs)
-    cos_sim = sim2(x=all_wvs_mat, y=this_wv_mat, method="cosine", norm="l2")
-}
-
+GLOVE <- "~/Data/glove.6B.100d.txt"
+# tidytext contains three sentiment lexicons. NRC contains binary indicators
+#       for a large set of words for 10 sentiment dimensions:
+#   anger anticipation disgust fear joy negative positive sadness surprise trust
 sentiment <- tidytext::get_sentiments('nrc')
-glove_sentiment <- proc_pretrained_vec(g6b_100, unique(sentiment$word))
+
+setwd("~/Projects/SPSP-TextAnalysis/TheoryDrivenTextAnalysis/")
+meta <- readRDS("./data/tdta_clean_house_data.RDS")
+
+# subset data (so I can run on baby chromebook)
+# comment following line for complete code
+meta <- meta[1:5000,]
+# make house data tidy
+tdta <- meta %>% 
+    select(doc_num, text) %>% 
+    unnest_tokens(word, text)
+
+# use text2vec::itoken, create_vocabulary and prune_vocabulary to quickly 
+# build vocabulary (max_size=20000)
+it = itoken(list(tdta$word))
+house_vocab = create_vocabulary(it)
+house_vocab = prune_vocabulary(house_vocab, vocab_term_max=20000)
+vocab <- unique(c(house_vocab$term, sentiment$word))
+# remove words not in vocabulary
+tdta <- tdta %>%
+    filter(word %in% vocab)
+
+# read in GloVe vectors (100-dim)
+# (1) create header ["word", X1, ..., X100]
+# (2) use read_table2 to read space-delimited file
+# (3) immediately filter embedding object to our vocabulary 
+cs <- c("word")
+for(i in 1:100) {cs <- c(cs, paste(c("X", i), collapse=""))}
+E <- read_table2(GLOVE, col_names = cs) %>%
+    filter(word %in% vocab)
+
+# compute average embeddings for (1) dictionary and (2) corpus (tidy)
+# (1) dictionary: join sentiment tibble (sentiment = [anger, sad,...],
+#                                        word = [...]) to embedding tibble, 
+#                 group_by and compute mean
+# (2) repeat for corpus: tdta is tidy (doc_num, word), join at word
 sentiment_embed <- sentiment %>%
-    left_join(glove_sentiment %>%
-                  rownames_to_column(var="word") %>%
-                  as_tibble(), 
-              by = "word") %>%
+    left_join(E, by = "word") %>%
     group_by(sentiment) %>%
     summarise_at(vars(-word), list(mean=~mean(., na.rm=TRUE)))
+corpus_embed <- tdta %>%
+    left_join(E, by='word') %>%
+    group_by(doc_num) %>%
+    summarise_at(vars(-word), list(mean=~mean(., na.rm=TRUE)))
 
-g6b_100 <- scan(file = "~/Data/glove.6B.100d.txt", what="", sep="\n")
-m <- as.matrix(sentiment_embed[2,2:101])
-find_sim_wvs(m, glove_sentiment, top_n_res = 200)
-sentiment_embed
+# compute similarities use text2vec::sim2
+#
+doc_nums <- corpus_embed$doc_num
+sentiment_idx <- sentiment_embed$sentiment
+sentiment_sims <- sim2(as.matrix(corpus_embed %>% select(-doc_num)),
+                       as.matrix(sentiment_embed %>% select(-sentiment)),
+                       method = 'cosine', norm = 'l2')
+
+# make similarity matrix into tibble                
+sentiment_sims <- as.data.frame(sentiment_sims)
+colnames(sentiment_sims) <- sentiment_idx 
+rownames(sentiment_sims) <- doc_nums
+sentiment_sims <- sentiment_sims %>%
+    rownames_to_column(var="doc_num") %>%
+    as_tibble()
+sentiment_sims
